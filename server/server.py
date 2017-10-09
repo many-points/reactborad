d50eeed9b8bb5871b6c887e289521d61fda3def6
Deloff <deloff@play-station-2.local>
2017-10-09 09:55:04 +0300

    template

---
+++ server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hello")
+def hello():
+    return "<h1>HELLO WORLD!</h1>"
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div class="container">
+        <div class="jumbotron">
+          <h1>Hello World!</h1>
+        </div>
+      </div>
+    );
+  }
+}
